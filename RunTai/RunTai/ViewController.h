790f04b8ed1e3d81b9be8e286389f49b46d6a50d
AJhin <ajhin@qq.com>
2016-03-14 16:50:08 +0800

    初始化项目

---
+++ RunTai/RunTai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunTai
+//
+//  Created by Joel Chen on 16/3/14.
+//  Copyright © 2016年 AJhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunTai/RunTai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RunTai
+//
+//  Created by Joel Chen on 16/3/14.
+//  Copyright © 2016年 AJhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

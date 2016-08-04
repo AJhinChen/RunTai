c961e090df08926608828847e117395b5da91d6e
Joel Chen <jin.chen@asiasoft.hk>
2016-08-04 21:09:09 +0800

    取消多推送

--- RunTai/RunTai/AppDelegate.m
+++ RunTai/RunTai/AppDelegate.m
@@ -81,9 +81,7 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))handler {
 }
 
 - (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    [AVOSCloudIM handleRemoteNotificationsWithDeviceToken:deviceToken constructingInstallationWithBlock:^(AVInstallation *currentInstallation) {
-        currentInstallation.deviceProfile = @"client_push_certificate";
-    }];
+    [AVOSCloudIM handleRemoteNotificationsWithDeviceToken:deviceToken];
 }
 
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
